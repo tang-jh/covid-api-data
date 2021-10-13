57491dab91ab84a09dcc3efb9b0fd2ccffd55e3c
junhao-tang <junhao.tang@outlook.com>
2021-10-13 23:53:40 +0800

    Landing page showing global stats on confirmed cases in last 5 days

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useReducer } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 import L from "leaflet";
 import geodata from "../Data/10m_admin_0";
 import urlcat from "urlcat";
+import { Button, Grid, Typography } from "@mui/material";
 
 let countryPoly;
 const BASEURL = `https://covid-api.mmediagroup.fr/v1`;
@@ -14,16 +15,16 @@ const Landing = () => {
   const [landingMap, setLandingMap] = useState(null);
   const [status, setStatus] = useState("idle");
   const [casesData, setCasesData] = useState([]);
-  const [vaccinesData, setVaccinesData] = useState();
-  const [historyData, setHistoryData] = useState();
-  
+  const [vaccinesData, setVaccinesData] = useState([]);
+  const [historyData, setHistoryData] = useState([]);
+  const [overlay, setOverlay] = useState([]);
+
   const fetchData = async (category, setMethod) => {
     setStatus("pending");
     try {
       const res = await fetch(urlcat(BASEURL, category));
       const json = await res.json();
       setStatus("resolved");
-      console.log("processjson", processJson(category)(json));
       setMethod(processJson(category)(json));
     } catch (error) {
       setStatus("error");
@@ -32,23 +33,26 @@ const Landing = () => {
 
   const processJson = (datatype, obj) => {
     switch (datatype) {
-        case CASES: return ((obj)=>{
-            const arr = [];
+      case CASES:
+        return (obj) => {
+          const arr = [];
           for (const k in obj) {
             if (obj[k].All.abbreviation) {
               arr.push({
                 country: k,
                 isoA2: obj[k].All.abbreviation,
                 confirmed: obj[k].All.confirmed,
-                deaths:obj[k].All.deaths,
-                population: obj[k].All.population
+                deaths: obj[k].All.deaths,
+                population: obj[k].All.population,
               });
             }
           }
-          return arr;});
-        
-          case VACCINES: return ((obj)=>{
-            const arr = [];
+          return arr;
+        };
+
+      case VACCINES:
+        return (obj) => {
+          const arr = [];
           for (const k in obj) {
             if (obj[k].All.abbreviation) {
               arr.push({
@@ -56,30 +60,112 @@ const Landing = () => {
                 isoA2: obj[k].All.abbreviation,
                 population: obj[k].All.population,
                 administered: obj[k].All.administered,
-                vaccinated:obj[k].All.people_vaccinated,
-                percent_vaccinated: ((obj[k].All.people_vaccinated/obj[k].All.population)*100).toFixed(1)
+                vaccinated: obj[k].All.people_vaccinated,
+                percent_vaccinated: parseFloat(
+                  (
+                    (obj[k].All.people_vaccinated / obj[k].All.population) *
+                    100
+                  ).toFixed(1)
+                ),
               });
             }
           }
-          return arr;});
-          case HISTORY: return (obj) => {
-            const arr = [];
-            for (const k in obj) {
-              if (obj[k].All.abbreviation) {
-                arr.push({
-                  country: k,
-                  isoA2: obj[k].All.abbreviation,
-                  population: obj[k].All.population,
-                  dates: obj[k].All.dates,
-                });
-              }
+          return arr;
+        };
+      case HISTORY:
+        return (obj) => {
+          const arr = [];
+          for (const k in obj) {
+            if (obj[k].All.abbreviation) {
+              arr.push({
+                country: k,
+                isoA2: obj[k].All.abbreviation,
+                population: obj[k].All.population,
+                dates: obj[k].All.dates,
+              });
             }
-            return arr;
-          };
-          default: return obj;
-        }
+          }
+          return arr;
+        };
+      default:
+        return obj;
     }
-  
+  };
+
+  console.log(geodata.features[3]);
+
+  const processGeodata = (geoFeatures) => {
+    const arr = [];
+    let item = {};
+    for (const f of geoFeatures) {
+      // for (const v of vaccinesData) {
+      //   if (f.properties.ISO_A2 === v.isoA2) {
+      //     item = {
+      //       ...f,
+      //       properties: {
+      //         ...f.properties,
+      //         vaccinated: v.vaccinated,
+      //         percent_vaccinated: v.percent_vaccinated,
+      //       },
+      //     };
+      //     // arr.push(item);
+      //     // item = {};
+      //   }
+      // }
+      for (const h of historyData) {
+        if (f.properties.ISO_A2 === h.isoA2) {
+          item = {
+            ...f,
+            properties: {
+              ...f.properties,
+              fivedays:
+                h.dates[indexAt(h.dates, 0)] - h.dates[indexAt(h.dates, 4)],
+              tendays:
+                h.dates[indexAt(h.dates, 0)] - h.dates[indexAt(h.dates, 9)],
+            },
+          };
+          arr.push(item);
+          item={};
+        }
+      }
+    }
+    // for (const c of casesData) {
+    //   if (f.properties.ISO_A2 === c.isoA2) {
+    //     console.log("f.properties.ISO_A2", f.properties.ISO_A2);
+    //     item = {
+    //       ...f,
+    //       properties: {
+    //         ...f.properties,
+    //         confirmed: c.confirmed,
+    //         confirmed_index: c.confirmed / c.population,
+    //         deaths: c.deaths,
+    //         deaths_index: c.deaths/c.population,
+    //         population: c.population,
+    //       },
+    //     };
+    //   }
+    console.log("ProcessGeoData", arr);
+    return arr;
+  };
+
+  const polyStyle = (currmax) => (feature) => {
+    return {
+      fillOpacity: feature.properties.fivedays/currmax ,
+    };
+  };
+
+  const indexAt = (obj, index) => {
+    const keys = Object.keys(obj);
+    return keys[index];
+  };
+
+  const onEachFeature = (feature, layer) => {
+    if (feature.properties.fivedays) {
+      layer.bindPopup(`Cases in five days ${feature.properties.fivedays}`);
+      layer.on("mouseover", (e)=>{e.target.openPopup()});
+      layer.on("mouseout", (e)=>{e.target.closePopup()});
+    }
+  }
 
   useEffect(() => {
     fetchData(CASES, setCasesData);
@@ -88,29 +174,44 @@ const Landing = () => {
   }, []);
 
   useEffect(() => {
-    if (!landingMap) return;
+    setOverlay(processGeodata(geodata.features));
+  }, [historyData]);
 
-    // if (polylayer) {
-    //   map.removeLayer(polylayer);
-    // }
-    countryPoly = L.geoJSON(geodata).addTo(landingMap);
-  }, [landingMap]);
+  useEffect(() => {
+    if (!landingMap) return;
+    if (countryPoly) {
+      landingMap.removeLayer(countryPoly);
+    }
+
+    const fivedays = overlay.map((item) =>
+      parseInt(item.properties.fivedays)
+    );
+    const maxFivedays = Math.max(...fivedays);
+
+    countryPoly = L.geoJSON(overlay, { style: polyStyle(maxFivedays), onEachFeature:onEachFeature }).addTo(
+      landingMap
+    );
+  }, [landingMap, overlay]);
 
   return (
     <div>
-      <h2>Landing page</h2>
-      <MapContainer
-        id="landing-map"
-        center={[46.8, 8.2]}
-        zoom={2}
-        scrollWheelZoom={true}
-        whenCreated={setLandingMap}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      <Typography gutterBottom={true} align="center" variant="h5">
+        Global Statistics on COVID-19
+      </Typography>
+      <Grid container justifyContent="center">
+        <MapContainer
+          id="landing-map"
+          center={[12.24, 1.56]}
+          zoom={2}
+          scrollWheelZoom={true}
+          whenCreated={setLandingMap}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </Grid>
     </div>
   );
 };
